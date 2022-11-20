25f07d83db3e4ca728e46af00bc66cecacc2812b
AnanaSsSsSsS <elias.gold@bk.ru>
2022-11-20 20:12:09 +0300

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by macbook on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
